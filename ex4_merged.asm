31dc4f13c770c85e4d94651059ee63751a6ea5b0
Mor Paz <1912.mor@gmail.com>
2020-11-07 10:45:55 +0000

    Finish q4, add tests

--- ex4_merged.asm
+++ ex4_merged.asm
@@ -1,9 +1,7 @@
-.global _start
-
-.section .text
-_start:
-  #Write your code here
-  # value already exists
+.text
+.global main
+main:
+    # write your code here
     # value already exists
     # root is empty
     # traverse till leaf and add
@@ -45,22 +43,94 @@ RIGHT:
     jmp LOOP
 
 ADDL:
-#    movq new_node, %rax
-#    movq %rax, 8(%rdx)
     movq $new_node, 8(%rdx)
     jmp EXIT
 
 
 ADDR:
-#    movq new_node, %rax
-#    movq %rax, 16(%rdx)
     movq $new_node, 16(%rdx)
     mov 16(%rdx), %r9
     jmp EXIT
     
 ADD:    
-#    movq new_node, %rax
-#    movq %rax, (root)
     movq $new_node, (root)
     
 EXIT:
+    
+
+  cmpq $A, (root)
+  jne bad_exit
+
+  movq $A, %rbx
+  cmpq $20, (%rbx)
+  jne bad_exit
+  cmpq $B, 8(%rbx)
+  jne bad_exit
+  cmpq $C, 16(%rbx)
+  jne bad_exit
+
+  movq $B, %rbx
+  cmpq $-4, (%rbx)
+  jne bad_exit
+  cmpq $0, 8(%rbx)
+  jne bad_exit
+  cmpq $D, 16(%rbx)
+  jne bad_exit
+
+  movq $C, %rbx
+  cmpq $26, (%rbx)
+  jne bad_exit
+  cmpq $new_node, 8(%rbx)
+  jne bad_exit
+  cmpq $0, 16(%rbx)
+  jne bad_exit
+
+  movq $D, %rbx
+  cmpq $-3, (%rbx)
+  jne bad_exit
+  cmpq $0, 8(%rbx)
+  jne bad_exit
+  cmpq $0, 16(%rbx)
+  jne bad_exit
+
+  movq $new_node, %rbx
+  cmpq $25, (%rbx)
+  jne bad_exit
+  cmpq $0, 8(%rbx)
+  jne bad_exit
+  cmpq $0, 16(%rbx)
+  jne bad_exit
+
+  movq $60, %rax
+  movq $0, %rdi
+  syscall
+
+bad_exit:
+  movq $60, %rax
+  movq $1, %rdi
+  syscall
+
+.section .data
+root:
+        .quad   A
+A:
+        .quad   20
+        .quad   B
+        .quad   C
+B:
+        .quad   -4
+        .quad   0
+        .quad   D
+C:
+        .quad   26
+        .quad   0
+        .quad   0
+D:
+        .quad   -3
+        .quad   0
+        .quad   0
+
+new_node:
+        .quad   25
+        .quad   0
+        .quad   0
